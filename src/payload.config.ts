5f60afbfd6876b7f13d27fbaed69213f07184d4e
juliademichel <jdemichel.jd@gmail.com>
2026-03-28 20:43:01 +0100

    Fix mongodb connection

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -18,6 +18,11 @@ import NewsletterSubscribers from './collections/NewsletterSubscribers'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
+console.log('=== DEBUG ENV ===')
+console.log(buildConfig)
+console.log('DATABASE_URL:', process.env.DATABASE_URL)
+console.log(Products)
+
 export default buildConfig({
   serverURL: 'http://localhost:3000',
   admin: {
@@ -54,42 +59,12 @@ export default buildConfig({
 /*
 import { buildConfig } from 'payload'
 
-console.log('=== DEBUG ENV ===')
-console.log(buildConfig)
-console.log('DATABASE_URI:', process.env.DATABASE_URI)
-console.log('PAYLOAD_SECRET:', process.env.PAYLOAD_SECRET)
+cconsole.log('PAYLOAD_SECRET:', process.env.PAYLOAD_SECRET)
 console.log('Current working directory:', process.cwd())
 console.log('=================')
-
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-
-import { Users } from './collections/Users'        
-import Products from './collections/Products'
-import Orders from './collections/Orders'
-import Founders from './collections/Founders'
-import FAQ from './collections/FAQ'
-import  { Media } from './collections/Media'
-import NewsletterSubscribers from './collections/NewsletterSubscribers'
 
 export default buildConfig({
   serverURL: 'http://localhost:3000',
   editor: lexicalEditor(),
-  admin: {
-    user:  'users',
-  },
-  collections: [
-    Users,
-    Products,
-    Orders,
-    Founders,
-    FAQ,
-    Media,
-    NewsletterSubscribers,
-  ],
-  db: mongooseAdapter({
-    url: process.env.DATABASE_URI as string,
-  }),
-  secret: process.env.PAYLOAD_SECRET as string,
-})
+  
 */
