af1238fea9c4b6df08649d25751755eadd900213
juliademichel <julia.demichel@hotmail.fr>
2026-05-07 12:28:52 +0800

    orders exports working?

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
-// import { importExportPlugin } from '@payloadcms/plugin-import-export'
+import { importExportPlugin } from '@payloadcms/plugin-import-export'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
